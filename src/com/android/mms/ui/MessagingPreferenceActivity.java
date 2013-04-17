LineageOS/android_packages_apps_Mms

Mms: Enable SoftBank emoji encoding
This adds an option to enable SoftBank emoji encoding when inserting
emoji into SMS/MMS message.

There is nothing wrong with the default Unicode emoji encoding other
than the fact that most Japanese carriers does not support it as they
have their own coding conversion tables. The option toggles to SoftBank
encoding, the decoding both Unicode and SoftBank-codeed emoji is
alredy working correctly (both code tables are already part of the
src/com/android/mms/util/EmojiParser.java). Unicode encoding will
stay the default. No translations this time...

Change-Id: I53754a2b06093bb2fa9e5414fa306c1dbe367b34
Committer: Samir Ibradzic <sibradzic@gmail.com>

src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -77,6 +77,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
 
     // Emoji
     public static final String ENABLE_EMOJIS             = "pref_key_enable_emojis";
+    public static final String SOFTBANK_EMOJIS           = "pref_key_enable_softbank_encodidng";
 
     // Unicode
     public static final String UNICODE_STRIPPING            = "pref_key_unicode_stripping";

PurityROM/platform_packages_apps_Mms

src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -75,6 +75,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
 
     // Emoji
     public static final String ENABLE_EMOJIS             = "pref_key_enable_emojis";
+    public static final String SOFTBANK_EMOJIS           = "pref_key_enable_softbank_encoding";
 
     // Unicode
     public static final String UNICODE_STRIPPING            = "pref_key_unicode_stripping";